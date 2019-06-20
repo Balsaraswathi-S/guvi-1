ca809c4f25ae695f88c4ec99fea46beb133c5812
Balsaraswathi-S <41724427+Balsaraswathi-S@users.noreply.github.com>
2019-06-20 10:19:01 +0530

    Create vowels.py

---
+++ programs/vowels.py
@@ -0,0 +1,7 @@
+s=input()
+if (s=='a'or s=='A'or s=='e' or s=='E'or s=='i' or s=='I'or s=='o' or s=='O' or s=='u' or s=='U'):
+  print("Vowels")
+elif(s.isalpha()):
+  print("Consonant")
+else:
+  print("Inavalid")
